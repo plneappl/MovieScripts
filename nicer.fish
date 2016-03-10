aaf9a8d7e26e09bd56a44c9be83ec428f9a2184e
plneappl <simon.wegendt@stundent.uni-tuebingen.de>
2016-03-10 15:33:22 +0100

    Canonize season/episode Only process files, not directories with '.' in the name

--- nicer.fish
+++ nicer.fish
@@ -26,7 +26,11 @@ set deletions $deletions   dvdrip hdtv repack bdrip webrip proper
 set deletionChars          '\(' '\)' '\[' '\]'
 
 for i in $searchIn
+  if [ ! -f $i ]
+    continue
+  end
   set fn (basename $i)
+  echo $fn
   set extension '.'(echo $fn | sed -e 's:^.*\.::')
   set name ' '(echo $fn | sed -e 's:\.[^\.]*$::')' '
   
@@ -46,29 +50,46 @@ for i in $searchIn
   end
 
   #uppercase each word
-  set nicerName (echo $nicerName | sed -r 's/\<./\U&/g')
+  set nicerName (echo $nicerName | sed -r 's:\<.:\U&:g')
+
+  #detect ID
+  set id       (echo $nicerName | sed -r 's:^.* s([0-9][0-9])e([0-9][0-9]) .*$: S\1E\2 :I')       #s01e01
+  set origId   (echo $nicerName | sed -r 's:^.* (s[0-9][0-9]e[0-9][0-9]) .*$: \1 :I')
+  if [ $id = $nicerName ]
+    set id     (echo $nicerName | sed -r 's:^.* ([0-9])([0-9]{2}) .*$: S0\1E\2 :I')               #101
+    set origId (echo $nicerName | sed -r 's:^.* ([0-9])([0-9]{2}) .*$: \1\2 :I')
+  end
+  if [ $id = $nicerName ]
+    set id     (echo $nicerName | sed -r 's:^.* ([0-9]{2})([0-9]{2}) .*$: S\1E\2 :I')             #1001
+    set origId (echo $nicerName | sed -r 's:^.* ([0-9]{2})([0-9]{2}) .*$: \1\2 :I')
+  end
+  if [ $id = $nicerName ]
+    echo "ID couldn't be detected"
+  else
+    echo '"'$origId'" -> "'$id'"'
+  end
+
+  #replace ID with S01E01
+  set nicerName (echo $nicerName | sed -r "s:$origId:$id:")
 
   #remove all duplicate space
   set nicerName (echo $nicerName | sed -r 's: +: :g')
 
   #remove space at the start/end of the file name
   set nicerName (echo $nicerName | sed -r 's:^ +::')
-  set nicerName (echo $nicerName | sed -r 's: +$::')$extension
+  set nicerName (echo $nicerName | sed -r 's: +$::')
 
-  set destination $searchPath/$nicerName
+  set destination $searchPath/$nicerName$extension
 
   if [ $nicerName ]
     #what we are going to do
-    echo $fn
-    echo $nicerName
+    echo $nicerName$extension
     echo '------------'
   
     #do it
     mv $i $destination
   else 
-    echo "Something went wrong... We didn't process"
-    echo $fn 
-    echo "because we deleted the name (whoops!)"
+    echo "Something went wrong... We didn't rename the file because we would have renamed it to $nicerName (whoops!)"
     echo '------------'
   end
 end
--- renamer.fish
+++ renamer.fish
@@ -18,22 +18,25 @@ end
 
 
 
-for i in $searchIn                                                          #search for 
-  set id (echo $i | sed -e 's:^.*\(s[0-9][0-9]e[0-9][0-9]\).*$:\1:I')       #s01e01
-  if [ $id = $i ]
-    set id (echo $i | sed -e 's:^.*\([0-9]\)\([0-9]\{2\}\).*$:s0\1e\2:I')     #101
-  end
-  if [ $id = $i ]
-    set id (echo $i | sed -e 's:^.*\([0-9]\{2\}\)\([0-9]\{2\}\).*$:s\1e\2:I')   #1001
-  end
-  if [ $id = $i ]
-    echo "couldn't find a match for:"
-    echo "\t$i"
-    echo '------------'
-    continue
-  end
+for i in $searchIn                                                          
   #to get the extension, delete everything before the point; sed is greedy so this is fine.
-  set extension '.'(echo $i | sed -e 's/.*\.//')
+  set extension '.'(echo $fn | sed -e 's:^.*\.::')
+  set name (basename $i $extension)
+
+  #detect ID
+  set id       (echo $name | sed -r 's:^.*s([0-9][0-9])e([0-9][0-9]).*$:S\1E\2:I')        #s01e01
+  if [ $id = $name ]
+    set id     (echo $name | sed -r 's:^.*([0-9])([0-9]{2}).*$:S0\1E\2:I')                #101
+    end
+  if [ $id = $name ]
+    set id     (echo $name | sed -r 's:^.*([0-9]{2})([0-9]{2}).*$:S\1E\2:I')              #1001
+    end
+  if [ $id = $name ]
+    echo "ID couldn't be detected"
+  else
+    echo '"'$id'"'
+  end
+  
   #find all files with our ID, for each, check if it's a directory, if so take that directory's name
   set fullName (for candidate in (find $searchPath/ -iname "*$id*") 
     if [ -d $candidate ] 
