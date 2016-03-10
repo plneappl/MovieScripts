89b7ead1624c50a3386169ce9004d385434a71a9
plneappl <simon.wegendt@stundent.uni-tuebingen.de>
2016-03-10 12:00:26 +0100

    more keywords

--- nicer.fish
+++ nicer.fish
@@ -21,7 +21,7 @@ end
 #all keywords to delete, case insensitive, spaces instead of [._-]
 set deletions              german dubbed x264 bluray web ac3 ac3md rip dts hd readnfo multi 'dd5 1' 'h 264' h264 dub '\(1\)' 
 set deletions $deletions   wmv dvd ts 'blu ray' dd51 'aac2 0' aac avc remux xvid tv avi mp4 'dd2 0' 'read nfo' internal ituneshd
-set deletions $deletions   dvdrip hdtv repack bdrip webrip
+set deletions $deletions   dvdrip hdtv repack bdrip webrip proper
 
 set deletionChars          '\(' '\)' '\[' '\]'
 
